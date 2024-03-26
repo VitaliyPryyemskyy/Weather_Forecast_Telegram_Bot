316bcb11f25616db3fab5b9b31907667315c3d95
Vitaliy <hykhyk666@gmail.com>
2024-03-26 13:08:55 +0100

    chore: upload to git hub

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/classic")
+def classic():
+    return render_template("classic.html")
+
+
+@app.route("/for_kids")
+def for_kids():
+    return render_template("for_kids.html")
+
+
+@app.route("/non_alcohol")
+def non_alcohol():
+    return render_template("non_alcohol.html")
+
+
+@app.route("/shots")
+def shots():
+    return render_template("shots.html")
+
+
+@app.route("/handle_form", methods=["GET", "POST"])
+def handle_form():
+
+    if request.method == "POST":
+        first_name = request.form.get("first_name", "")
+        last_name = request.form.get("last_name", "")
+        with open("database.txt", "r") as file:
+            for line in file:
+                if first_name in line and last_name in line:
+                    return f"This name already taken {render_template('form.html')}"
+        with open("database.txt", "a") as file:
+            file.write(f"{first_name} {last_name}\n")
+        return f"{first_name}, {render_template('complete_registration.html')} "
+    else:
+        return render_template("form.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        first_name = request.form.get("first_name", "")
+        last_name = request.form.get("last_name", "")
+
+        with open("database.txt", "r") as file:
+            for line in file:
+                if first_name in line and last_name in line:
+                    return f"welcome {first_name} {render_template('index.html')}"
+                elif first_name == False and last_name == False:
+                    return f"Enter name and lastname{render_template('login.html')}"
+            return f" Invalid username {render_template('login.html')}"
+    else:
+        return render_template("login.html")
