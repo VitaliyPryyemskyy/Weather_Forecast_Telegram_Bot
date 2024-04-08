51fa88baa73f5e64b3cade9a21732ebf2ac28eb0
Vitaliy <hykhyk666@gmail.com>
2024-04-08 19:23:47 +0200

    build: upload cocktail web

--- app.py
+++ app.py
@@ -5,58 +5,63 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 @app.route("/classic")
 def classic():
-    return render_template("classic.html")
+    return render_template('classic.html')
 
 
 @app.route("/for_kids")
 def for_kids():
-    return render_template("for_kids.html")
+    return render_template('for_kids.html')
 
 
 @app.route("/non_alcohol")
 def non_alcohol():
-    return render_template("non_alcohol.html")
+    return render_template('non_alcohol.html')
 
 
 @app.route("/shots")
 def shots():
-    return render_template("shots.html")
+    return render_template('shots.html')
 
 
-@app.route("/handle_form", methods=["GET", "POST"])
+@app.route("/handle_form", methods=['GET', 'POST'])
 def handle_form():
 
-    if request.method == "POST":
-        first_name = request.form.get("first_name", "")
-        last_name = request.form.get("last_name", "")
-        with open("database.txt", "r") as file:
+    if request.method == 'POST':
+        first_name = request.form.get('first_name', '')
+        last_name = request.form.get('last_name', '')
+        with open('database.txt', 'r') as file:
             for line in file:
                 if first_name in line and last_name in line:
                     return f"This name already taken {render_template('form.html')}"
-        with open("database.txt", "a") as file:
-            file.write(f"{first_name} {last_name}\n")
+        with open('database.txt', 'a') as file:
+            file.write(f'\n{first_name} {last_name}\n')
         return f"{first_name}, {render_template('complete_registration.html')} "
     else:
-        return render_template("form.html")
+        return render_template('form.html')
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        first_name = request.form.get("first_name", "")
-        last_name = request.form.get("last_name", "")
+    if request.method == 'POST':
 
-        with open("database.txt", "r") as file:
+        first_name = request.form.get('first_name', '')
+        last_name = request.form.get('last_name', '')
+
+        with open('database.txt', 'r') as file:
+
             for line in file:
+
                 if first_name in line and last_name in line:
-                    return f"welcome {first_name} {render_template('index.html')}"
-                elif first_name == False and last_name == False:
-                    return f"Enter name and lastname{render_template('login.html')}"
-            return f" Invalid username {render_template('login.html')}"
+
+                    return render_template('login.html', first_name=first_name)
+
+            return render_template('login.html', error_message='wrong name')
+
     else:
-        return render_template("login.html")
+
+        return render_template('login.html')
