dea8ddf595fdfd90360aec360c8d1508be0eb7af
Vitaliy <hykhyk666@gmail.com>
2024-06-26 22:44:05 +0200

    build:add bot

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# weather_bot
+# Weather Forecast Telegram Bot
+
+
+## Features
+- City Weather: Get weather updates by sending the name of a city.
+- Location-Based Weather:  Share your location to get weather updates for your current location (works only in the     mobile Telegram app, not in the desktop version).
+- Predefined City Buttons: Quick access buttons for Berlin, London, and Kyiv.
+- City Image: Provides an image of the requested city using the Unsplash API.
+- Multilingual Support: Uses I18nMiddleware for internationalization.
+
+## Prerequisites
+- Python 3.x
+- Telegram Bot Token
+- OpenWeatherMap API Key
+- Unsplash API Key
+
+## Installation
+
+1. ### Clone the repository
+git clone git@github.com:VitaliyPryyemskyy/Weather_Forecast_Telegram_Bot.git
+cd Weather_Forecast_Telegram_Bot
+
+2. ### Configure API Keys
+Open config.py and replace the placeholder values with your actual API keys.
+- OpenWeatherMap API: https://openweathermap.org/api
+- Unsplash API: https://unsplash.com/developers
+- Telegram Bot Registration: https://docs.expertflow.com/cx/4.3/telegram-bot-creation-guide
+
+3. ### Run the setup script in terminal
+./build.sh
+
+This script creates a virtual environment, installs necessary packages, and launches the application.
+
+## Bot Interactions
+- Predefined City Buttons: Click on Berlin, London, or Kyiv buttons to get the weather forecast.
+- Location Sharing: Share your location to receive weather updates for your current location.
+- City Name Input: Type and send the name of any city to get the weather forecast.
+
+## Run as a Python Script
+python main.py
--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -1,82 +1,126 @@
 import requests
 from datetime import datetime
-from config import tg_bot_token, open_wether_token
+from config import tg_bot_token, open_weather_token, unsplash_access_key
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor 
-import datetime
-
+from aiogram.utils import executor
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-
+from aiogram.dispatcher.filters import Text
+from aiogram.contrib.middlewares.i18n import I18nMiddleware
+import datetime
 
 bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
 
 
+i18n = I18nMiddleware("bot", "locales")
+dp.middleware.setup(i18n)
+_ = i18n.gettext
+
+
 def keyboard():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-    berlin_button = KeyboardButton('Berlin')
-    kyiv_button = KeyboardButton('Kyiv')
-    exit_button = KeyboardButton('👨‍💻 EXIT')
-    markup.add(exit_button, berlin_button, kyiv_button)
+    berlin_button = KeyboardButton("Berlin")
+    london_button = KeyboardButton("London")
+    kyiv_button = KeyboardButton("Kyiv")
+    my_location_button = KeyboardButton("My Location", request_location=True)
+    exit_button = KeyboardButton("👨‍💻 EXIT")
+    markup.add(
+        berlin_button, kyiv_button, london_button, my_location_button, exit_button
+    )
     return markup
 
-# @dp.message_handler(lambda message: message.text == '🇩🇪 Berlin')
-# async def berlin_weather(message: types.Message):
-#     await get_weather(message, 'Berlin')
 
-# @dp.message_handler(lambda message: message.text == '🇺🇦 Kyiv')
-# async def kyiv_button(message: types.Message):
-#     await get_weather(message, 'Kyiv')
-
-
-@dp.message_handler(lambda message: message.text == '👨‍💻 EXIT')
-async def exit_command(message: types.Message):
-    await message.reply('Exiting...')
-    await bot.close()
-    
-
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
     markup = keyboard()
-    await message.reply('Send a city name:', reply_markup=markup)
+    await message.reply("Send a city name or share your location:", reply_markup=markup)
 
+
+@dp.message_handler(Text(equals="👨‍💻 EXIT"))
+async def exit_command(message: types.Message):
+    await message.reply("Exiting...")
+    await bot.session.close()
+
+
+@dp.message_handler(content_types=["location"])
+async def get_weather_by_location(message: types.Message):
+    latitude = message.location.latitude
+    longitude = message.location.longitude
+    try:
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+        await send_weather_info(message, data)
+    except Exception as e:
+        print(f"Error getting weather by location: {e}")
+        await message.reply("Failed to get weather for your location 🤷🏼‍♂️")
 
 
 @dp.message_handler()
 async def get_weather(message: types.Message):
     try:
         r = requests.get(
-            f'https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_wether_token}&units=metric')
+            f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+        )
         data = r.json()
-
-        city = data['name']
-        cur_weather = data['main']['temp']
-        feels_like = data['main']['feels_like']
-        sky = data['weather'][0]['description']
-        humidity = data['main']['humidity']
-        pressure = data['main']['pressure']
-        wind = data['wind']['speed']
-        sunrise_timestamp = datetime.datetime.fromtimestamp(
-            data["sys"]["sunrise"])
-
-        sunset_timestamp = datetime.datetime.fromtimestamp(
-            data["sys"]["sunset"])
-        lenght_of_day = datetime.datetime.fromtimestamp(
-            data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
-            data["sys"]["sunrise"])
-        await message.reply(f"Today is: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}\n"
-                            f'Weather in {city}\n'
-                            f'Temperature: {cur_weather}°C\nFeels like: {feels_like}\n'
-                            f'Sky is: {sky}\nHumidity: {humidity}%\nPressure: {pressure}mm\nWind speed: {wind}m/sec\n'
-                            f'Sunrise at {sunrise_timestamp}\nSunset at {sunset_timestamp}\nLenght of day is:{lenght_of_day}')
+        await send_weather_info(message, data)
     except Exception as e:
-        await message.reply(f"I don't know  a city {message.text} 🤷🏼‍♂️")
-   
+        print(f"Error getting weather by city name: {e}")
+        await message.reply(f"I don't know a city {message.text} 🤷🏼‍♂️")
 
 
+async def send_weather_info(message: types.Message, data):
+    try:
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+        feels_like = data["main"]["feels_like"]
+        sky = data["weather"][0]["description"]
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_time = datetime.datetime.fromtimestamp(data["sys"]["sunrise"]).strftime(
+            "%H:%M:%S"
+        )
+        sunset_time = datetime.datetime.fromtimestamp(data["sys"]["sunset"]).strftime(
+            "%H:%M:%S"
+        )
+        length_of_day = datetime.datetime.fromtimestamp(
+            data["sys"]["sunset"]
+        ) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+
+        # Get city image from Unsplash
+        image_url = get_city_image(city)
+
+        await message.reply(
+            f"Today is: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}\n"
+            f"Weather in {city}\n"
+            f"Temperature: {cur_weather}°C\nFeels like: {feels_like}\n"
+            f"Sky is: {sky}\nHumidity: {humidity}%\nPressure: {pressure} mm\nWind speed: {wind} m/sec\n"
+            f"Sunrise at {sunrise_time}\nSunset at {sunset_time}\nLength of day is: {length_of_day}\n"
+            f"[City Image]({image_url})",
+            parse_mode="Markdown",
+        )
+    except Exception as e:
+        print(f"Error parsing weather data: {e}")
+        await message.reply("Failed to parse weather data 🤷🏼‍♂️")
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp)
+def get_city_image(city):
+    try:
+        response = requests.get(
+            f"https://api.unsplash.com/search/photos?query={city}&client_id={unsplash_access_key}"
+        )
+        data = response.json()
+        if data["results"]:
+            return data["results"][0]["urls"]["small"]
+        else:
+            return "No image found"
+    except Exception as e:
+        print(f"Error getting city image: {e}")
+        return "No image found"
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
